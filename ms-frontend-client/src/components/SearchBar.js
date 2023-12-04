6ade916198599fd7cb9865c278061e6c0fd8f995
shayan <f190976@nu.edu.pk>
2023-12-04 13:44:45 +0500

    Again changed

--- ms-frontend-client/src/components/SearchBar.js
+++ ms-frontend-client/src/components/SearchBar.js
@@ -150,7 +150,14 @@ export default function CustomizedInputBase(props) {
             loading,
           }) => (
             <>
-              
+              <InputBase
+                {...getInputProps({
+                  placeholder: "Enter delivery address",
+                })}
+                className={classes.input}
+                inputProps={{
+                  "aria-label": "search google maps for delivery address",
+                }}
               />
               <div className={classes.results}>
                 {loading ? <div>Getting Results...</div> : null}
